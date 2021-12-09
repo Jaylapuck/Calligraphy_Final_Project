84445e558d97bd350d3a2109bc65ba446daeb995
Jeremy Forest <ethylamide@icloud.com>
2021-12-08 19:02:57 -0500

    fixed cors again, two  different projects, gonna delete the old one.

--- Calligraphy/Controllers/FormController.cs
+++ Calligraphy/Controllers/FormController.cs
@@ -5,12 +5,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Mvc;
 using HttpGetAttribute = System.Web.Http.HttpGetAttribute;
 using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
 
 namespace Calligraphy.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class FormController : ApiController
     {
         // GET: Form
