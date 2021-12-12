5c1f99739600d7ca2a18878edabf9dfbf13c1699
Jeremy Forest <ethylamide@icloud.com>
2021-12-11 20:34:13 -0500

    fix unit testing for .net core

---
+++ Calligraphy.Test/Form/FormControllerTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calligraphy.Business.Form;
+using Calligraphy.Controllers;
+using Calligraphy.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Calligraphy.Test.Form
+{
+    public class FormControllerTests
+    {
+        private readonly Mock<IFormService> _mockFormService;
+        private readonly FormController _formController;
+
+        public FormControllerTests()
+        {
+            _mockFormService = new Mock<IFormService>();
+            _formController = new FormController(_mockFormService.Object);
+        }
+
+        // TS2-TC1
+        [Fact]
+        // test get all api, returns list of forms
+        public void GetAll()
+        {
+            // Arrange
+            var forms = new List<FormEntity>
+            {
+                new FormEntity {  FormId = 1, ServiceType = "Form 1", Comments = "Description 1"},
+                new FormEntity { FormId = 2, ServiceType = "Form 2", Comments = "Description 2"}
+            };
+
+            _mockFormService.Setup(x => x.GetAll()).Returns(forms);
+
+            // Act
+            var result = _formController.Get();
+
+            // Assert
+            Assert.IsType<List<FormEntity>>(result);
+            Assert.Equal(2, result.Count());
+        }
+
+        // TS2-TC3
+        [Fact]
+        // test get all api, returns empty list
+        public void GetAll_ReturnsEmptyList()
+        {
+            // Arrange
+            var forms = new List<FormEntity>();
+
+            _mockFormService.Setup(x => x.GetAll()).Returns(forms);
+
+            // Act
+            var result = _formController.Get();
+
+            // Assert
+            Assert.IsType<List<FormEntity>>(result);
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        // TC2-TC2
+        public void Post_ReturnsOkResult()
+        {
+            // Arrange
+            var expected = new FormEntity();
+            _mockFormService.Setup(x => x.Create(expected)).Returns(true);
+
+            // Act
+            var actual = _formController.Post(expected);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(actual);
+        }
+
+        [Fact]
+        // TC2-TC4
+        public void Post_ReturnsBadRequest()
+        {
+            // Arrange
+            var expected = new FormEntity();
+            _mockFormService.Setup(x => x.Create(expected)).Returns(false);
+
+            // Act
+            var actual = _formController.Post(null);
+
+            // Assert
+            Assert.IsType<BadRequestResult>(actual);
+        }
+    }
+}
---
+++ Calligraphy.Test/Form/FormServiceTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calligraphy.Business.Form;
+using Calligraphy.Controllers;
+using Calligraphy.Data.Models;
+using Calligraphy.Data.Repo;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Calligraphy.Test.Form
+{
+    public class FormServiceTests
+    {
+        private readonly Mock<IFormRepo> _mockFormRepo;
+        private readonly FormService _formService;
+
+        public FormServiceTests()
+        {
+            _mockFormRepo = new Mock<IFormRepo>();
+            _formService = new FormService(_mockFormRepo.Object);
+        }
+
+        [Fact]
+        // TS2-TC5
+        public void GetAllForms()
+        {
+            // Arrange
+            var forms = new List<FormEntity>
+            {
+                new FormEntity {FormId = 1, ServiceType = "Service Type 1", Comments = "Comments 1"},
+                new FormEntity {FormId = 2, ServiceType = "Service Type 2", Comments = "Comments 2"},
+                new FormEntity {FormId = 3, ServiceType = "Service Type 3", Comments = "Comments 3"}
+            };
+            
+            // Act
+            _mockFormRepo.Setup(x => x.GetAll()).Returns(forms);
+            var result = _formService.GetAll();
+            
+            // Assert
+            Assert.Equal(3, result.Count());
+            
+        }
+        
+        [Fact]
+        // TS1-TC6
+        public void CreateForm()
+        {
+            // Arrange
+            var form = new FormEntity {FormId = 1, ServiceType = "Service Type 1", Comments = "Comments 1"};
+            
+            // Act
+            _mockFormRepo.Setup(x => x.Create(form));
+            _formService.Create(form);
+            
+            // Assert
+            _mockFormRepo.Verify(x => x.Create(form), Times.Once);
+        }
+    }
+}
