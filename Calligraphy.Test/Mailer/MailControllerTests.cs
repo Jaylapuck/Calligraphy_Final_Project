7b3102fde6c31fe36fe0302dcf75a5b2da2c0275
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2021-12-15 11:45:38 -0500

    Removed the upgrade assistant references and added to tests for exceptions

--- Calligraphy.Test/Mailer/MailControllerTests.cs
+++ Calligraphy.Test/Mailer/MailControllerTests.cs
@@ -1,10 +1,12 @@
 ﻿using Calligraphy.Controllers;
 using Calligraphy.Mailer.Model;
 using Calligraphy.Mailer.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,7 +35,7 @@ namespace Calligraphy.Test.Mailer
 
         [Fact]
         // Test to see that the controller can send an email
-        public async Task SendMailOkTest()
+        public async Task SendMailOkTestEmpty()
         {
             // Arrange 
             MailRequest email = new MailRequest();
@@ -46,6 +48,103 @@ namespace Calligraphy.Test.Mailer
 
             // Assert
             Assert.IsType<OkResult>(result);
+        }
+
+
+        [Fact]
+        // Test to see that the controller can send an email
+        public async Task SendMailOkTestNoSubject()
+        {
+            // Arrange 
+            MailRequest email = new MailRequest();
+            email.email = "email1";
+            email.body = "body1";
+            string filePath = @"..\..\..\Mailer\TestFiles\23784.png";
+            using var stream = new MemoryStream(File.ReadAllBytes(filePath).ToArray());
+            var formFile = new FormFile(stream, 0, stream.Length, "streamFile", filePath.Split(@"\").Last());
+            List<IFormFile> attachtments = new List<IFormFile>();
+            attachtments.Add(formFile);
+            email.attachtments = attachtments;
+
+
+            // Simulate the await property in the setup of the post method
+            _service.Setup(s => s.SendMailAsync(email)).Returns(async () => { await Task.Yield(); });
+
+            // Act
+            var result = await _controller.Send(email);
+
+            // Assert
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        // Test to see that the controller can send an email
+        public async Task SendMailOkTestNoBody()
+        {
+            // Arrange 
+            MailRequest email = new MailRequest();
+            email.email = "email1";
+            email.subject = "subject1";
+            string filePath = @"..\..\..\Mailer\TestFiles\23784.png";
+            using var stream = new MemoryStream(File.ReadAllBytes(filePath).ToArray());
+            var formFile = new FormFile(stream, 0, stream.Length, "streamFile", filePath.Split(@"\").Last());
+            List<IFormFile> attachtments = new List<IFormFile>();
+            attachtments.Add(formFile);
+            email.attachtments = attachtments;
+
+            // Simulate the await property in the setup of the post method
+            _service.Setup(s => s.SendMailAsync(email)).Returns(async () => { await Task.Yield(); });
+
+            // Act
+            var result = await _controller.Send(email);
+
+            // Assert
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public async Task SendMailOkTestNoAttachtments()
+        {
+            // Arrange 
+            MailRequest email = new MailRequest();
+            email.email = "email1";
+            email.subject = "subject1";
+            email.body = "body1";
+            email.attachtments = new List<IFormFile>();
+
+            // Simulate the await property in the setup of the post method
+            _service.Setup(s => s.SendMailAsync(email)).Returns(async () => { await Task.Yield(); });
+
+            // Act
+            var result = await _controller.Send(email);
+
+            // Assert
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public async Task SendMailBadRequestNoEmail()
+        {
+            // Arrange
+            MailRequest email = new MailRequest();
+            email.email = "";
+            email.subject = "subject1";
+            email.body = "body1";
+            string filePath = @"..\..\..\Mailer\TestFiles\23784.png";
+            using var stream = new MemoryStream(File.ReadAllBytes(filePath).ToArray());
+            var formFile = new FormFile(stream, 0, stream.Length, "streamFile", filePath.Split(@"\").Last());
+            List<IFormFile> attachtments = new List<IFormFile>();
+            attachtments.Add(formFile);
+            email.attachtments = attachtments;
+
+            _service.Setup(s => s.SendMailAsync(email)).Returns(async () => { await Task.Yield(); });
+
+            // Act
+            Func<Task> result = () => _controller.Send(email);
+
+            // Assert
+            var error = await Assert.ThrowsAsync<ArgumentNullException>(result);
+            Assert.Equal("Email not found", error.Message);
         }
     }
 }
