c17d60fbbc96b0f97cfc829880222497e1772372
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2021-12-29 16:01:39 -0500

    Test for getting a retrieval for services

--- Calligraphy.Data/Repo/Service/ServiceRepoImpl.cs
+++ Calligraphy.Data/Repo/Service/ServiceRepoImpl.cs
@@ -1,4 +1,6 @@
-﻿using Calligraphy.Data.Models;
+﻿using Calligraphy.Data.Config;
+using Calligraphy.Data.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +9,20 @@ namespace Calligraphy.Data.Repo.Service
 {
     public class ServiceRepoImpl : IServiceRepo
     {
+        private readonly CalligraphyContext _context;
+        private readonly DbContextOptions<CalligraphyContext> options;
+
+
+        public ServiceRepoImpl(CalligraphyContext context)
+        {
+            _context = context;
+        }
+
+        public ServiceRepoImpl()
+        {
+            _context = new CalligraphyContext(options);
+        }
+
         public IEnumerable<ServiceEntity> GetAll()
         {
             throw new NotImplementedException();
---
+++ Calligraphy.Test/EFRepoTests.cs
@@ -0,0 +1,38 @@
+﻿using Calligraphy.Data.Config;
+using Calligraphy.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calligraphy.Test
+{
+    public class EFRepoTests
+    {
+        protected DbContextOptions<CalligraphyContext> ContextOptions { get; }
+
+        public EFRepoTests(DbContextOptions<CalligraphyContext> contextOptions)
+        {
+            ContextOptions = contextOptions;
+
+            Seed();
+        }
+
+        private void Seed()
+        {
+            using (var context = new CalligraphyContext(ContextOptions))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                var service1 = new ServiceEntity { TypeName = Data.Enums.ServiceType.Calligraphy, StartingRate = 20.00f };
+
+                context.AddRange(service1);
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
