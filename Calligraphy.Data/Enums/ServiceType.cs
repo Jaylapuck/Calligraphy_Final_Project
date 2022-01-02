ffe736b09f50d1af0c5402d05f0463333d366cf5
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2022-01-02 18:23:24 -0500

    updated the enums to show strings rather than numbers

--- Calligraphy.Data/Enums/ServiceType.cs
+++ Calligraphy.Data/Enums/ServiceType.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Calligraphy.Data.Enums
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum ServiceType
     {
         Calligraphy,
