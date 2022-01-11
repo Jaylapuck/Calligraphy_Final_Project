328c58663b56d5d3e979e3cbaa6088c86ada2155
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2022-01-11 11:21:25 -0500

    Test for get all in controller

---
+++ Calligraphy/Controllers/ContractController.cs
@@ -0,0 +1,32 @@
+﻿using Calligraphy.Business.Contract;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace Calligraphy.Controllers
+{
+    [EnableCors("ApiCorsPolicy")]
+    [ApiController]
+    [Route("[controller]")]
+    public class ContractController : ControllerBase
+    {
+        private IContractService _contractService;
+
+        public ContractController(IContractService contractService)
+        {
+            _contractService = contractService;
+        }
+
+        [HttpGet]
+        [Produces(MediaTypeNames.Application.Json)]
+        [Route("/api/get")]
+        public IActionResult GetAllContracts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
